e5daa3d8f442ea4633b780d261778bb904006ef6
jun.shimoji <jun.shimoji@playnext-lab.co.jp>
2020-09-05 22:10:28 +0900

    選択状態を解除(cursorRight)、ショートカット付与

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,23 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.unorderedItemIndent', () => {
 
 		let editor:any = vscode.window.activeTextEditor; //activeなtexteditorのobject取得
-
-		let position = editor.selection.active;
-		let line = editor.selection.active.line;
-		let textLine = editor.document.lineAt(line).text; //lineのテキストを行ごと取得
+		let position   = editor.selection.active;
+		let line       = editor.selection.active.line;
+		let textLine   = editor.document.lineAt(line).text; //lineのテキストを行ごと取得
 
 		editor?.edit((builder: any) => {
-			let startPos = new vscode.Position(line, 0);
-			let endPos = new vscode.Position(line, textLine.length);
-			let myPos = new vscode.Selection(startPos, endPos);
+			let startPos     = position.with(position.line, 0);  //カーソル行の最初の位置
+			let endPos       = position.with(position.line, textLine.length);
+			let myPos        = new vscode.Selection(startPos, endPos);
+			let newPos       = new vscode.Selection(endPos, endPos);
 			builder.delete(myPos); // カーソルのある行を削除
-            builder.replace(editor?.selection, '* ' + textLine?.trim());
+            builder.replace(editor?.selection, '* ' + textLine?.trim()); // "*"を追加
+			editor.selection = newPos; // この状態だと選択状態になる
+			vscode.commands.executeCommand("cursorRight"); // カーソル右押すことで選択状態を解除する
         });
-		console.log('position: ', position);
-		console.log('line: ', line);
-		vscode.window.showInformationMessage(textLine);
 	});
 
 	let cmd2 = vscode.commands.registerCommand('extension.calc', () => {
